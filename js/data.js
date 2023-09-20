9f44fe10ccfcf0b89de0d090b803d89d19fc87d2
Omid Asadi <omiddzedd@gmail.com>
2023-09-20 21:13:50 +0000

    Completed all of issue #1's tasks.

--- js/data.js
+++ js/data.js
@@ -1,8 +1,15 @@
-/* exported data */
-
 let data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1,
 };
+
+const previousEntriesJSON = localStorage.getItem('javascript-local-storage');
+if (previousEntriesJSON !== null) {
+  data = JSON.parse(previousEntriesJSON);
+}
+window.addEventListener('beforeunload', function (event) {
+  const entriesJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', entriesJSON);
+});

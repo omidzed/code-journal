d01d8ee61e46617eb5c49ce65c6167299795a4c2
Omid Asadi <omiddzedd@gmail.com>
2023-09-22 00:04:22 +0000

    added anchor to navbar, but need to properly add an eventHandler to it. #2

--- js/main.js
+++ js/main.js
@@ -63,28 +63,40 @@ document.addEventListener('DOMContentLoaded', function (event) {
   }
 });
 
-// function toggleNoEntries(){
-//   if ()
+function toggleNoEntries() {
+  const $noEntriesView = document.querySelector('div[data-view="no-entries"]');
+  if (data.entries.length !== 0) {
+    $noEntriesView.setAttribute('class', 'hidden');
+  } else {
+    $noEntriesView.setAttribute('class', 'view');
+  }
+}
 
-// }
+const $entryFormView = document.querySelector('div[data-view="entry-form"]');
+// eslint-disable-next-line no-unused-vars
+const $entriesView = document.querySelector('div[data-view="entries"]');
+// eslint-disable-next-line no-unused-vars
+function viewSwap(view) {
+  if (view === 'entry-form') {
+    $entryFormView.setAttribute('class', 'view');
+  } else {
+    toggleNoEntries();
+  }
+}
 
-// const $tabContainer = document.querySelector('.tab-container');
-// const $tabList = document.querySelectorAll('.tab');
-// const $viewList = document.querySelectorAll('.view');
+// $entryForm.addEventListener('submit', function (event) {
+//   event.preventDefault();
+//   const entry = {
+//     entryId: data.nextEntryId,
+//     title: $titleInput.value,
+//     photoUrl: $photoUrlInput.value,
+//     notes: $notesText.value,
+//   };
+//   data.entries.unshift(renderEntry(entry));
+//   data.nextEntryId++;
+//   $entryForm.reset();
+//   $entryImage.src = './images/placeholder-image-square.jpg';
+//   if (viewSwap(view) === ''){
 
-// console.log($tabContainer);
-// console.log($tabList);
-// console.log($viewList);
-
-// $tabContainer.addEventListener('click', function (event) {
-//   if (event.target.matches('.tab')) {
-//     for (let i = 0; i < $tabList.length; i++) {
-//       if ($tabList[i] === event.target && $tabList[i].className === 'tab') {
-//         $tabList[i].className = 'tab active';
-//       } else $tabList[i].className = 'tab';
-//     const dataView = event.target.getAttribute('data-view');
-//       for (let i = 0; i < $viewList.length; i++) {
-//         if ($viewList[i].getAttribute('data-view') === dataView) {
-//           $viewList[i].className = 'view';
-//         } else $viewList[i].className = 'view hidden';
-//       }
+//   }
+// });
